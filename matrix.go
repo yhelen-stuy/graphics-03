68082a68bdf84dfdcc22d090a3041fd9c1b9c007
Helen Ye <00yhelen@gmail.com>
2018-02-14 11:54:06 -0500

    Make matrix fxn

--- matrix.go
+++ matrix.go
@@ -7,3 +7,16 @@ type Matrix struct {
 	rows, cols int
 	lastcol    int
 }
+
+func MakeMatrix(rows, cols int) *Matrix {
+	mat = make([][]float64, rows)
+	for i := range mat {
+		mat[i] = make([]float64, cols)
+	}
+	matrix = &Matrix{
+		mat:  mat,
+		rows: rows,
+		cols: cols,
+	}
+	return matrix
+}
