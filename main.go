df4140142522ae2f1eb287bd2cb3b0b0fa0d15a7
Helen Ye <00yhelen@gmail.com>
2018-02-16 12:28:57 -0500

    Actually finish matrix mult

--- main.go
+++ main.go
@@ -7,16 +7,26 @@ import (
 func main() {
 	var mat *Matrix
 	var id *Matrix
-	mat = MakeMatrix(2, 2)
-	id = MakeMatrix(2, 2)
-	id.Ident()
-	fmt.Println(id)
+	mat = MakeMatrix(4, 4)
+	id = MakeMatrix(4, 4)
 	for i := range mat.mat {
 		for j := range mat.mat[i] {
 			mat.mat[i][j] = 2
 		}
 	}
+	for i := range id.mat {
+		for j := range id.mat[i] {
+			id.mat[i][j] = float64(i)
+		}
+	}
 	fmt.Println(mat)
-	mat.Mult(*id)
+	fmt.Println(id)
+	mat, _ = mat.Mult(*id)
 	fmt.Println(mat)
+
+	// fmt.Println("adding 1,2,3")
+	// mat.Ident()
+	// mat.AddPoint(1.0, 2.0, 3.0)
+	// fmt.Println(mat)
+
 }
--- matrix.go
+++ matrix.go
@@ -25,48 +25,58 @@ func MakeMatrix(rows, cols int) *Matrix {
 	return matrix
 }
 
-func (matrix Matrix) Ident() error {
-	if matrix.rows != matrix.cols {
+func (m Matrix) Ident() error {
+	if m.rows != m.cols {
 		return errors.New("Error: not a square mat")
 	}
-	for i := range matrix.mat {
-		for j := range matrix.mat[i] {
+	for i := range m.mat {
+		for j := range m.mat[i] {
 			if i == j {
-				matrix.mat[i][j] = 1.0
+				m.mat[i][j] = 1.0
 			} else {
-				matrix.mat[i][j] = 0.0
+				m.mat[i][j] = 0.0
 			}
 		}
 	}
 	return nil
 }
 
-// Method on a matrix, that modifies the m2, the receiver
+// Method on a matrix, that modifies m2, the receiver
 // DOES NOT modify m1
 // Performs function:
 // m1 * m2 -> m2
-func (m2 Matrix) Mult(m1 Matrix) error {
+func (m2 Matrix) Mult(m1 Matrix) (*Matrix, error) {
 	// TODO: Make sure it works on non identity matrix
-	if m2.rows != m1.cols {
-		return errors.New("Error: dimensions incompatible")
+	if m1.rows != m2.cols {
+		return nil, errors.New("Error: dimensions incompatible")
 	}
+	prod := MakeMatrix(m2.rows, m1.cols)
 	value := 0.0
 	for r1 := 0; r1 < m1.rows; r1++ {
 		for c2 := 0; c2 < m2.cols; c2++ {
 			for rc := 0; rc < m1.cols; rc++ {
-				value += m1.mat[r1][rc] * m2.mat[rc][c2]
+				value += m2.mat[r1][rc] * m1.mat[rc][c2]
 			}
-			m2.mat[r1][c2] = value
-			value = 0
+			prod.mat[r1][c2] = value
+			value = 0.0
 		}
 	}
-	return nil
+	return prod, nil
 }
 
-func (matrix Matrix) String() string {
+func (m Matrix) AddPoint(x, y, z float64) {
+	p := make([]float64, 4)
+	p[0] = x
+	p[1] = y
+	p[2] = z
+	p[3] = 1
+	m.mat = append(m.mat, p)
+}
+
+func (m Matrix) String() string {
 	var buf bytes.Buffer
-	for i := range matrix.mat {
-		for _, c := range matrix.mat[i] {
+	for i := range m.mat {
+		for _, c := range m.mat[i] {
 			buf.WriteString(fmt.Sprintf("%.2f ", c))
 		}
 		buf.WriteString("\n")
